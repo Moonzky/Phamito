4a7020597453b3fd181ec2813c33844e54c6b411
Night Sky <rociosoledadmartinez728@gmail.com>
2024-09-30 00:16:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let slider_index= 0;
+showslide(slider_index)
+
+
+function changeslide(n) {
+    showslide (slider_index += n)
+}
+
+function showslide(n) {
+    let slides= document.querySelectorAll('.carrousel-slider img')
+    if (n >=slides.length) {
+        slider_index= 0
+    }
+     if (n <0) {
+        slider_index= slides.length -1
+     }
+     slides.forEach (slide=> slide.classList.remove('slide-active'))
+     slides[slider_index].classList.add('slide-active')
+}
+
+setInterval( ()=>{
+    changeslide(1)
+},5000
+
+)
