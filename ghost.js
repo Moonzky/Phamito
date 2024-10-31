8791dcf277464c790f5222ab9646e81b557e8fcd
benjamin zamora <benjazamo@gmail.com>
2024-10-30 22:13:00 -0300

    cambiukys

--- ghost.js
+++ ghost.js
@@ -11,3 +11,23 @@ function changeghost(n) {
     ghost_nav[n-1].classList.add('ghost-nav-active')
     ghost[n-1].classList.add('ghost-active')
 }
+
+let slider_index = 0;
+showslide(slider_index)
+
+
+function changeslide(n) {
+    showslide (slider_index += n)
+}
+
+function showslide(n) {
+    let slides= document.querySelectorAll('.carrousel-slider > img')
+    if (n >=slides.length) {
+        slider_index= 0
+    }
+    if (n <0) {
+        slider_index= slides.length -1
+    }
+    slides.forEach (slide=> slide.classList.remove('ghost-slider-active'))
+    slides[slider_index].classList.add('ghost-slider-active')
+}
